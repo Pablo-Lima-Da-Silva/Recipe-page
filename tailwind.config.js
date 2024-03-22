1a38fa2990fd2320e9e791d7bc604e452ec14b6b
Pablo Silva <pablosilvaaluno@outlook.com>
2024-03-22 14:44:37 -0300

    TailWindCSS
    Adicionando cores e arrumando codigo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Nutmeg': '#854632',
+        'dark-raspberry': '#7b284f',
+        'rose-white': '#fff5fa',
+        'eggshell': '#f3e6d8',
+        'light-grey': '#e4ded8',
+        'wenge-brown': '5f574e',
+        'dark-charcoal': '#302d2c',
+      },
+    },
   },
   plugins: [],
-}
-
+};
